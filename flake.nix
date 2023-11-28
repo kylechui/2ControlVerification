5b516a437f27bca4048c39d7d9f500e787fe70cf
Kyle Chui <kyle.chui+github@pm.me>
2023-11-27 19:11:31 -0800

    feat: Add `QuantumLib` to flake dependencies.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,39 @@
 {
   description = "Local development environment for 2ControlVerification";
   inputs = {
-    # Pinned version of nixpkgs containing Coq 8.17.0
+    # Pinned version of nixpkgs containing Coq 8.15.0
     nixpkgs.url =
-      "github:nixos/nixpkgs/6eef602bdb2a316e7cf5f95aeb10b2ff0a97e4a5";
+      "github:nixos/nixpkgs/6e3a86f2f73a466656a401302d3ece26fba401d9";
   };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      pname = "quantumlib";
+      owner = "inQWIRE";
+    in let
+      coq_quantum_lib = pkgs.coqPackages.mkCoqDerivation {
+        inherit pname owner;
+        src = pkgs.fetchFromGitHub {
+          inherit owner;
+          repo = "QuantumLib";
+          rev = "main";
+          sha256 = "sha256-evIxqyjiOc+HkIl9Ft+7DZHYx74v7UGG5mHg6dlG6oA=";
+        };
+        useDune = true;
+        buildInputs = [ pkgs.dune_3 pkgs.ocaml ];
+        installPhase = ''
+          runHook preInstall
+          dune install coq-${pname} --prefix=$out --mandir=$out/man
+          mv $out/lib/coq $out/lib/TEMPORARY
+          mkdir $out/lib/coq/
+          mv $out/lib/TEMPORARY $out/lib/coq/${pkgs.coq.coq-version}
+          runHook postInstall
+        '';
+      };
     in {
       devShell.${system} =
-        pkgs.mkShell { nativeBuildInputs = [ pkgs.coqPackages.coq ]; };
+        pkgs.mkShell { nativeBuildInputs = [ pkgs.coq coq_quantum_lib ]; };
     };
 }
