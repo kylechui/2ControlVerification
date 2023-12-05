d2487db8a49061e8d0d438d635b766f5a7a5cc48
Neil Vaishampayan <neiluv@icloud.com>
2023-12-04 19:44:25 -0800

    Continued work on a9, last step proof is fiinished.

--- UnitaryMatrices.v
+++ UnitaryMatrices.v
@@ -559,6 +559,18 @@ split.
     reflexivity.
 Qed.
 
+Lemma complex_split: forall (a b: C),
+a = b  -> fst a = fst b /\ snd a = snd b.
+Proof. 
+intros.
+split.
+rewrite H.
+reflexivity.
+rewrite H.
+reflexivity.
+Qed.
+
+
 Lemma squared_norm_eq_0_implies_0: forall (a: C), 
 a^* * a = 0 -> a = 0.
 Proof.
@@ -566,8 +578,114 @@ intros.
 apply c_proj_eq.
 unfold Cconj in *; unfold Cmult in *.
 simpl in *.
-inversion H.
-lca.
+{
+    cut ((fst a * fst a - - snd a * snd a)%R = 0).
+    cut ((fst a * fst a)%R = 0 -> fst a = 0).
+    cut (((fst a * fst a - - snd a * snd a)%R = 0 -> (fst a * fst a)%R = 0)).
+    intros.
+    apply H1.
+    apply H0.
+    apply H2.
+    {
+        rewrite <- Ropp_mult_distr_l.
+        assert (Step1: (fst a * fst a - - (snd a * snd a) = fst a * fst a + (snd a * snd a))%R). 
+        { 
+            field.
+        }
+        rewrite Step1.
+        set (b := fst a).
+        set (c := snd a).
+        set (d := (b * b)%R).
+        set (e := (c*c)%R).
+        assert (Step2: 0 <= d). 
+        {
+            apply Rle_0_sqr.
+        }
+        assert (Step3: 0 <= e). 
+        {
+            apply Rle_0_sqr.
+        }
+        apply Rplus_eq_0_l.
+        apply Step2.
+        apply Step3.
+    }
+    {
+        apply Rsqr_0_uniq.
+    }
+    {
+        inversion H.
+        reflexivity.
+    }
+}
+{
+    cut ((fst a * fst a - - snd a * snd a)%R = 0).
+    cut ((snd a * snd a)%R = 0 -> snd a = 0).
+    cut (((fst a * fst a - - snd a * snd a)%R = 0 -> (snd a * snd a)%R = 0)).
+    intros.
+    apply H1.
+    apply H0.
+    apply H2.
+    {
+        rewrite <- Ropp_mult_distr_l.
+        assert (Step1: (fst a * fst a - - (snd a * snd a) = fst a * fst a + (snd a * snd a))%R). 
+        { 
+            field.
+        }
+        rewrite Step1.
+        set (b := fst a).
+        set (c := snd a).
+        set (d := (b * b)%R).
+        set (e := (c*c)%R).
+        rewrite Rplus_comm.
+        apply Rplus_eq_0_l.
+        apply Rle_0_sqr.
+        apply Rle_0_sqr.
+    }
+    {
+        apply Rsqr_0_uniq.
+    }
+    {
+        inversion H.
+        reflexivity.
+    }
+}
+Qed.
+
+Lemma sum_of_adjoints_re_nonneg: forall (b c d: C),
+Re (b ^* * b + (c ^* * c + d ^* * d)) >= 0.
+Proof.
+intros.
+unfold Re, Cplus.
+simpl.
+rewrite <- Ropp_mult_distr_l. rewrite <- Ropp_mult_distr_l. rewrite <- Ropp_mult_distr_l.
+assert (Step1: (fst b * fst b - - (snd b * snd b) = fst b * fst b + snd b * snd b)%R). field. 
+rewrite Step1. clear Step1.
+assert (Step2: (fst c * fst c - - (snd c * snd c) = fst c * fst c + snd c * snd c)%R). field. 
+rewrite Step2. clear Step2.
+assert (Step3: (fst d * fst d - - (snd d * snd d) = fst d * fst d + snd d * snd d)%R). field. 
+rewrite Step3. clear Step3.
+apply Rle_ge.
+apply Rplus_le_le_0_compat.
+apply Rplus_le_le_0_compat.
+apply Rle_0_sqr.
+apply Rle_0_sqr.
+apply Rplus_le_le_0_compat.
+apply Rplus_le_le_0_compat.
+apply Rle_0_sqr.
+apply Rle_0_sqr.
+apply Rplus_le_le_0_compat.
+apply Rle_0_sqr.
+apply Rle_0_sqr.
+Qed.
+
+Lemma sum_of_adjoints_im_0: forall (b c d: C),
+Im (b ^* * b + (c ^* * c + d ^* * d)) = 0.
+Proof.
+intros.
+unfold Im, Cplus.
+simpl.
+lra.
+Qed.
 
 Lemma sum_of_squared_norms_eq_0_implies_0: forall (a b c d: C), 
 a ^* * a + b ^* * b + c ^* * c + d ^* * d = 0 -> a = 0 /\ b = 0 /\ c = 0 /\ d= 0.
@@ -580,9 +698,89 @@ split.
     rewrite <- Cplus_assoc.
     set (f := a^* * a).
     set (g := b ^* * b + (c ^* * c + d ^* * d)).
-    intros H.
+    cut (f+g = 0 -> f = 0).
+    cut (f = 0 -> a = 0).
+    intros.
+    apply H.
+    apply H0.
+    apply H1.
+    apply squared_norm_eq_0_implies_0.
     apply sum_of_pos_c_is_0_implies_0.
+    apply conj_mult_re_is_nonneg.
+    apply conj_mult_im_is_0.
+    apply sum_of_adjoints_re_nonneg.
+    apply sum_of_adjoints_im_0.
 }
+split.
+{
+    revert H.
+    assert (Step1: a ^* * a + b ^* * b + c ^* * c + d ^* * d = b ^* * b + a ^* * a  + c ^* * c + d ^* * d). 
+    {
+        lca.
+    }
+    rewrite Step1.
+    rewrite <- Cplus_assoc.
+    rewrite <- Cplus_assoc.
+    set (f := b^* * b).
+    set (g := a ^* * a + (c ^* * c + d ^* * d)).
+    cut (f+g = 0 -> f = 0).
+    cut (f = 0 -> b = 0).
+    intros.
+    apply H.
+    apply H0.
+    apply H1.
+    apply squared_norm_eq_0_implies_0.
+    apply sum_of_pos_c_is_0_implies_0.
+    apply conj_mult_re_is_nonneg.
+    apply conj_mult_im_is_0.
+    apply sum_of_adjoints_re_nonneg.
+    apply sum_of_adjoints_im_0.
+}
+split.
+{
+    revert H.
+    assert (Step1: a ^* * a + b ^* * b + c ^* * c + d ^* * d = c ^* * c + a ^* * a + b ^* * b + d ^* * d). 
+    {
+        lca.
+    }
+    rewrite Step1.
+    rewrite <- Cplus_assoc.
+    rewrite <- Cplus_assoc.
+    set (f := c^* * c).
+    set (g := a ^* * a + (b ^* * b + d ^* * d)).
+    cut (f+g = 0 -> f = 0).
+    cut (f = 0 -> c = 0).
+    intros.
+    apply H.
+    apply H0.
+    apply H1.
+    apply squared_norm_eq_0_implies_0.
+    apply sum_of_pos_c_is_0_implies_0.
+    apply conj_mult_re_is_nonneg.
+    apply conj_mult_im_is_0.
+    apply sum_of_adjoints_re_nonneg.
+    apply sum_of_adjoints_im_0.
+}
+{
+    revert H.
+    rewrite Cplus_comm.
+    rewrite <- Cplus_assoc.
+    set (f := d^* * d).
+    set (g := a ^* * a + (b ^* * b + c ^* * c)).
+    cut (f+g = 0 -> f = 0).
+    cut (f = 0 -> d = 0).
+    intros.
+    apply H.
+    apply H0.
+    apply H1.
+    apply squared_norm_eq_0_implies_0.
+    apply sum_of_pos_c_is_0_implies_0.
+    apply conj_mult_re_is_nonneg.
+    apply conj_mult_im_is_0.
+    apply sum_of_adjoints_re_nonneg.
+    apply sum_of_adjoints_im_0.
+}
+Qed.
 
 
 
@@ -603,4 +801,4 @@ by_cell.
 
 lca.
 lma.
-ring. *).
+ring. *)
