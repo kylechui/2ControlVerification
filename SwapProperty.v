5bf245c303a79ac86c600878b9dd623c80de9081
Neil <neiluv@icloud.com>
2023-12-21 02:37:44 -0800

    removed all dependencies from swap property
    this way this file doesn't get recompiled when changing others.

--- SwapProperty.v
+++ SwapProperty.v
@@ -1,16 +1,20 @@
 Require Import QuantumLib.Quantum.
-From Proof Require Import GateHelpers.
-From Proof Require Import SwapHelpers.
 
+(* used no helper definitions to remove all dependences from this *)
 Property swap_helper : forall (U : Square 4),
   WF_Matrix U -> 
-  acgate U = swapab × bcgate U × swapab.
+  (I 2 ⊗ swap) × (U ⊗ I 2) × (I 2 ⊗ swap) = (swap ⊗ I 2) × (I 2 ⊗ U) × (swap ⊗ I 2).
 Proof.
   intros. 
   apply mat_equiv_eq.
-  apply WF_acgate. apply H.
-  apply WF_mult. apply WF_mult. apply WF_swapab. apply WF_bcgate. apply H. apply WF_swapab.
-  unfold acgate; unfold bcgate; unfold abgate.
+  apply WF_mult. apply WF_mult.
+  apply WF_kron. reflexivity. reflexivity. apply WF_I. apply WF_swap.
+  apply WF_kron. reflexivity. reflexivity. apply H. apply WF_I.
+  apply WF_kron. reflexivity. reflexivity. apply WF_I. apply WF_swap.
+  apply WF_mult. apply WF_mult.
+  apply WF_kron. reflexivity. reflexivity. apply WF_swap. apply WF_I.
+  apply WF_kron. reflexivity. reflexivity. apply WF_I. apply H.
+  apply WF_kron. reflexivity. reflexivity. apply WF_swap. apply WF_I.
   by_cell.
   (* The lma that YT used fails, so running lcas by cell. Still takes a while. *)
   lca. lca. lca. lca. lca. lca. lca. lca.
