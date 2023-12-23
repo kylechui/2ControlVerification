beeb902f70a4078f9f7b26cb680103f5efc51791
Kyle Chui <kyle.chui+github@pm.me>
2023-12-22 18:56:14 -0800

    feat: Add `coq-lsp` as dependency.

--- flake.nix
+++ flake.nix
@@ -1,32 +1,73 @@
 {
   description = "Local development environment for 2ControlVerification";
-  inputs = {
-    # Pinned version of nixpkgs containing Coq 8.15.0
-    nixpkgs.url =
-      "github:nixos/nixpkgs/6e3a86f2f73a466656a401302d3ece26fba401d9";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      quantumlib-version = "v1.1.0";
+      pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
+        coq = prev.coq_8_15;
+        coqPackages = prev.coqPackages_8_15;
+      });
+      coq-quantumlib-version = "v1.1.0";
+      coq-lsp-version = "v8.15";
     in let
-      coq_quantum_lib = pkgs.coqPackages.mkCoqDerivation {
+      coq-quantumlib = pkgs.coqPackages.mkCoqDerivation {
         pname = "quantumlib";
         owner = "inQWIRE";
         repo = "QuantumLib";
 
-        defaultVersion = quantumlib-version;
-        release.${quantumlib-version} = {
-          rev = "refs/tags/${quantumlib-version}";
+        defaultVersion = coq-quantumlib-version;
+        release.${coq-quantumlib-version} = {
+          rev = "refs/tags/${coq-quantumlib-version}";
           sha256 = "sha256-YVDFLYAMFEfimIa1D6399z6Lo7RgSRJXrgEp2IEDQ0E=";
         };
+        useDune = true;
+      };
 
-        useDune2 = true;
+      coq-lsp = pkgs.coqPackages.mkCoqDerivation {
+        pname = "lsp";
+        owner = "jishnusen";
+        repo = "coq-lsp";
+        defaultVersion = coq-lsp-version;
+        release.${coq-lsp-version} = {
+          rev = "1c5b5445c4f5b81ce2ab69cb9febb0c2318c9ac6";
+          sha256 = "sha256-7/K1BmN3IuPUPwfoFHO26LOugi3sbhHdwESY0Xotcco=";
+        };
+        installPhase = ''
+          runHook preInstall
+          dune install coq-lsp --prefix=$out
+          wrapProgram $out/bin/coq-lsp --prefix OCAMLPATH : $OCAMLPATH
+          runHook postInstall
+        '';
+        useDune = true;
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        propagatedBuildInputs = [ pkgs.coqPackages.serapi ]
+          ++ (with pkgs.coq.ocamlPackages; [
+            camlp-streams
+            dune-build-info
+            menhir
+            uri
+            yojson
+          ]);
       };
     in {
-      devShell.${system} =
-        pkgs.mkShell { packages = [ pkgs.coq coq_quantum_lib ]; };
+      devShell.${system} = pkgs.mkShell {
+        packages = [
+          pkgs.coq
+          pkgs.coq.ocamlPackages.ocaml
+          pkgs.coq.ocamlPackages.dune_3
+          coq-quantumlib
+          coq-lsp
+        ];
+        propagatedBuildInputs = [ pkgs.coqPackages.serapi ]
+          ++ (with pkgs.coq.ocamlPackages; [
+            camlp-streams
+            dune-build-info
+            menhir
+            uri
+            yojson
+          ]);
+      };
     };
 }
