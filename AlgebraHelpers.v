f96e1b8396524d04b4e41d2ae64b6f8ea620b4d8
Neil <neiluv@icloud.com>
2023-12-28 16:20:54 -0800

    started simplifying/refactoring algebrahelpers

--- AlgebraHelpers.v
+++ AlgebraHelpers.v
@@ -3,15 +3,10 @@ Lemma conj_mult_re_is_nonneg: forall (a: C),
 Re (a^* * a) >= 0.
 Proof.
 intros.
-unfold Re; unfold Cconj; unfold Cmult.
-simpl.
+unfold Re, Cconj, Cmult. simpl.
 rewrite <- Ropp_mult_distr_l.
-assert (Step1: (fst a * fst a - - (snd a * snd a) = fst a * fst a + (snd a * snd a))%R).
-{
-    field.
-}
-rewrite Step1.
-intros.
+unfold Rminus.
+rewrite Ropp_involutive.
 apply Rle_ge.
 apply Rplus_le_le_0_compat.
 apply Rle_0_sqr.
@@ -22,9 +17,17 @@ Lemma conj_mult_im_is_0: forall (a: C),
 Im (a^* * a) = 0.
 Proof.
 intros.
-unfold Im; unfold Cconj; unfold Cmult.
-simpl.
-lra.
+unfold Im, Cconj, Cmult.
+simpl. lra.
+Qed.
+
+Lemma complex_split: forall (a b: C),
+a = b  -> fst a = fst b /\ snd a = snd b.
+Proof.
+intros.
+split.
+rewrite H. reflexivity.
+rewrite H. reflexivity.
 Qed.
 
 Lemma sum_of_pos_c_is_0_implies_0: forall (a b: C),
@@ -36,44 +39,26 @@ unfold Re in *; unfold Im in *.
 unfold Cplus in H3.
 rewrite H0 in H3.
 rewrite H2 in H3.
-cut ((fst a + fst b = 0)%R).
-intros.
+apply complex_split in H3.
+destruct H3 as [H3 _].
 split.
-+
-    apply c_proj_eq.
-    simpl.
-    revert H4.
+{
+    apply c_proj_eq. simpl.
+    revert H3.
     apply Rplus_eq_0_l.
-    apply Rge_le.
-    apply H.
-    apply Rge_le.
-    apply H1.
+    apply Rge_le. apply H.
+    apply Rge_le. apply H1.
     apply H0.
-+
-    apply c_proj_eq.
-    simpl.
-    revert H4.
+}
+{
+    apply c_proj_eq. simpl.
+    revert H3.
     rewrite Rplus_comm.
     apply Rplus_eq_0_l.
-    apply Rge_le.
-    apply H1.
-    apply Rge_le.
-    apply H.
+    apply Rge_le. apply H1.
+    apply Rge_le. apply H.
     apply H2.
-+
-    inversion H3.
-    reflexivity.
-Qed.
-
-Lemma complex_split: forall (a b: C),
-a = b  -> fst a = fst b /\ snd a = snd b.
-Proof.
-intros.
-split.
-rewrite H.
-reflexivity.
-rewrite H.
-reflexivity.
+}
 Qed.
 
 
@@ -85,75 +70,29 @@ apply c_proj_eq.
 unfold Cconj in *; unfold Cmult in *.
 simpl in *.
 {
-    cut ((fst a * fst a - - snd a * snd a)%R = 0).
-    cut ((fst a * fst a)%R = 0 -> fst a = 0).
-    cut (((fst a * fst a - - snd a * snd a)%R = 0 -> (fst a * fst a)%R = 0)).
-    intros.
-    apply H1.
-    apply H0.
-    apply H2.
+    apply Rsqr_0_uniq.
+    assert (((fst a * fst a - - snd a * snd a)%R = 0 -> (fst a * fst a)%R = 0)).
     {
         rewrite <- Ropp_mult_distr_l.
-        assert (Step1: (fst a * fst a - - (snd a * snd a) = fst a * fst a + (snd a * snd a))%R).
-        {
-            field.
-        }
-        rewrite Step1.
-        set (b := fst a).
-        set (c := snd a).
-        set (d := (b * b)%R).
-        set (e := (c*c)%R).
-        assert (Step2: 0 <= d).
-        {
-            apply Rle_0_sqr.
-        }
-        assert (Step3: 0 <= e).
-        {
-            apply Rle_0_sqr.
-        }
-        apply Rplus_eq_0_l.
-        apply Step2.
-        apply Step3.
+        unfold Rminus. rewrite Ropp_involutive.
+        apply Rplus_eq_0_l. apply Rle_0_sqr. apply Rle_0_sqr.
     }
-    {
-        apply Rsqr_0_uniq.
-    }
-    {
-        inversion H.
-        reflexivity.
-    }
+    apply H0.
+    inversion H.
+    reflexivity.   
 }
 {
-    cut ((fst a * fst a - - snd a * snd a)%R = 0).
-    cut ((snd a * snd a)%R = 0 -> snd a = 0).
-    cut (((fst a * fst a - - snd a * snd a)%R = 0 -> (snd a * snd a)%R = 0)).
-    intros.
-    apply H1.
-    apply H0.
-    apply H2.
+    apply Rsqr_0_uniq. 
+    assert (((fst a * fst a - - snd a * snd a)%R = 0 -> (snd a * snd a)%R = 0)).
     {
         rewrite <- Ropp_mult_distr_l.
-        assert (Step1: (fst a * fst a - - (snd a * snd a) = fst a * fst a + (snd a * snd a))%R).
-        {
-            field.
-        }
-        rewrite Step1.
-        set (b := fst a).
-        set (c := snd a).
-        set (d := (b * b)%R).
-        set (e := (c*c)%R).
+        unfold Rminus. rewrite Ropp_involutive.
         rewrite Rplus_comm.
-        apply Rplus_eq_0_l.
-        apply Rle_0_sqr.
-        apply Rle_0_sqr.
+        apply Rplus_eq_0_l. apply Rle_0_sqr. apply Rle_0_sqr.
     }
-    {
-        apply Rsqr_0_uniq.
-    }
-    {
-        inversion H.
-        reflexivity.
-    }
+    apply H0.
+    inversion H.
+    reflexivity. 
 }
 Qed.
 
