867f66015c56143b2267c082393f0a065830b9b9
Kyle Chui <kyle.chui+github@pm.me>
2024-01-03 00:26:34 -0800

    refactor(M4.2): Partial progress for `a <> 0 /\ b <> 0` case.

--- Main.v
+++ Main.v
@@ -47,9 +47,10 @@ Lemma m4_2 : forall (u0 u1 : C),
     let beta : Vector 2 := Q × ∣0⟩ in
     let beta_perp := Q × ∣1⟩ in
     (exists (P0 P1 : Square 2),
-    WF_Unitary P0 -> WF_Unitary P1 ->
-    I 2 ⊗ I 2 ⊗ (beta × beta†) .+ P0 ⊗ P1 ⊗ (beta_perp × beta_perp†) = ccu (diag2 u0 u1))
-      <-> u0 = 1 /\ u1 = 1.
+      WF_Unitary P0 /\
+      WF_Unitary P1 /\
+      I 2 ⊗ I 2 ⊗ (beta × beta†) .+ P0 ⊗ P1 ⊗ (beta_perp × beta_perp†) = ccu (diag2 u0 u1))
+    <-> u0 = 1 /\ u1 = 1.
 Proof.
   intros.
   assert (WF_Q : WF_Matrix Q).
@@ -168,11 +169,61 @@ Proof.
         rewrite beta_mult_0_0 in H2.
         rewrite beta_perp_mult_1_1 in H2.
         admit. (* TODO: Eigenvalues! *)
-      * destruct H2 as [P0 [P1 H2]].
+      * destruct H2 as [P0 [P1 [WF_P0 [WF_P1 H2]]]].
         apply (f_equal (fun f => f × (∣1⟩ ⊗ ∣1⟩ ⊗ beta))) in H2.
-        admit. (* TODO: a <> 0 /\ b <> 0 case! *)
-      admit.
-    admit.
+        assert (perp_inner_prod_0 : ⟨beta, beta_perp⟩ = C0).
+        {
+          unfold inner_product, beta_perp, beta.
+          rewrite Mmult_adjoint.
+          rewrite <- Mmult_assoc.
+          rewrite Mmult_assoc with (A := ⟨0∣).
+          destruct H1.
+          rewrite H3.
+          rewrite Mmult_1_r. 2: apply (WF_bra 0).
+          rewrite Mmult01.
+          unfold Zero; simpl.
+          reflexivity.
+        }
+        rewrite Mmult_plus_distr_r in H2.
+        assert (step1 : I 2 ⊗ I 2 ⊗ (beta × (beta) †) × (∣1⟩ ⊗ ∣1⟩ ⊗ beta) = ∣1⟩ ⊗ ∣1⟩ ⊗ beta).
+        {}
+        assert (step2 : P0 ⊗ P1 ⊗ (beta_perp × (beta_perp) †) × (∣1⟩ ⊗ ∣1⟩ ⊗ beta) = Zero). {}
+        rewrite step1, step2 in H2; clear step1 step2.
+        rewrite Mplus_0_r in H2.
+        assert (step3 : ccu (diag2 u0 u1) × (∣1⟩ ⊗ ∣1⟩ ⊗ beta) = ∣1⟩ ⊗ ∣1⟩ ⊗ (diag2 u0 u1 × beta)). {}
+        rewrite step3 in H2 at 1; clear step3.
+        do 2 rewrite kron_assoc in H2.
+        do 2 apply kron_1_cancel_l in H2.
+        assert (u0_is_1 : u0 = C1).
+        {
+          apply (f_equal (fun f => f 0%nat 0%nat)) in H2.
+          unfold diag2, Mmult in H2; simpl in H2.
+          replace (beta 0%nat 0%nat) with a in H2 by reflexivity.
+          rewrite Cmult_0_l in H2.
+          rewrite Cplus_0_l in H2.
+          rewrite Cplus_0_r in H2.
+          apply Cmult_cancel_r with (a := a).
+          exact a_nonzero.
+          rewrite <- H2.
+          rewrite Cmult_1_l.
+          reflexivity.
+        }
+        assert (u1_is_1 : u1 = C1).
+        {
+          apply (f_equal (fun f => f 1%nat 0%nat)) in H2.
+          unfold diag2, Mmult in H2; simpl in H2.
+          replace (beta 1%nat 0%nat) with b in H2 by reflexivity.
+          rewrite Cmult_0_l in H2.
+          repeat rewrite Cplus_0_l in H2.
+          apply Cmult_cancel_r with (a := b).
+          exact b_nonzero.
+          rewrite <- H2.
+          rewrite Cmult_1_l.
+          reflexivity.
+        }
+        split.
+        ** exact u0_is_1.
+        ** exact u1_is_1.
   - intros.
     destruct H2.
     exists (I 2), (I 2).
