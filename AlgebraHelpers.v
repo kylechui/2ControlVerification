52b1dc4833e60e5220d939ea41c3847a2fed90a4
Kyle Chui <kyle.chui+github@pm.me>
2024-01-17 21:53:54 -0800

    feat(AlgebraHelpers): Implement basic version of `Csqrt_sqrt`.

--- AlgebraHelpers.v
+++ AlgebraHelpers.v
@@ -271,8 +271,115 @@ apply H.
 trivial.
 Qed.
 
+(* If b = 0, then the value is real, in which case we should be using sqrt *)
+(* Could do casework here, but I think it might make `lca` fail *)
 Definition Csqrt (x : C) : C :=
   let norm := Cmod x in
-  match x with
-  | (a, b) => (sqrt ((norm + a) / 2), sqrt ((norm - a) / 2))
-  end.
+  let a := fst x in
+  let b := snd x in
+    (sqrt ((norm + a) / 2), Rabs b / b * sqrt ((norm - a) / 2))%R.
+
+Lemma Csqrt_sqrt : forall (x : C),
+  snd x <> 0 -> Csqrt x * Csqrt x = x.
+Proof.
+  intros.
+  assert (H0 : forall (r s : R), r <= sqrt (r * r + s * s)).
+  {
+    intros.
+    destruct (Rle_dec 0 r).
+    - rewrite <- sqrt_square at 1. 2: assumption.
+      apply sqrt_le_1_alt.
+      rewrite <- Rplus_0_r at 1.
+      apply Rplus_le_compat_l.
+      apply Rle_0_sqr.
+    - apply Rnot_le_lt in n.
+      apply Rlt_le in n.
+      pose proof (sqrt_pos (r * r + s * s)) as H0.
+      apply Rle_trans with (r2 := 0); assumption.
+  }
+  assert (nonneg1 : 0 <= (Cmod x - fst x) / 2).
+  {
+    apply Rmult_le_pos.
+    rewrite <- Rplus_opp_r with (r := fst x).
+    apply Rplus_le_compat_r.
+    unfold Cmod; unfold pow; repeat rewrite Rmult_1_r.
+    apply H0.
+    lra.
+  }
+  assert (nonneg2 : 0 <= (Cmod x + fst x) / 2).
+  {
+    apply Rmult_le_pos.
+    rewrite <- Rplus_opp_l with (r := fst x).
+    apply Rplus_le_compat_r.
+    unfold Cmod; unfold pow; repeat rewrite Rmult_1_r.
+    replace (fst x * fst x)%R with ((- fst x) * (- fst x))%R by lra.
+    apply H0.
+    lra.
+  }
+  assert (nonneg3 : 0 <= fst x * fst x + snd x * snd x).
+  {
+    apply Rplus_le_le_0_compat.
+    apply Rle_0_sqr.
+    apply Rle_0_sqr.
+  }
+  unfold Csqrt, Cmult; simpl.
+  rewrite sqrt_sqrt; auto.
+  replace (Rabs (snd x) / snd x * √ ((Cmod x - fst x) / 2) *
+  (Rabs (snd x) / snd x * √ ((Cmod x - fst x) / 2)))%R with ((Rabs (snd x) / snd x) * (Rabs (snd x) / snd x) * (√ ((Cmod x - fst x) / 2) * √ ((Cmod x - fst x) / 2)))%R by lra.
+  rewrite sqrt_sqrt; auto.
+  assert (step1 : forall (r : R), (Rabs r * Rabs r = r * r)%R).
+  {
+    intros.
+    destruct (Rle_dec 0 r).
+    - rewrite Rabs_right; auto.
+      lra.
+    - apply Rnot_le_lt in n.
+      rewrite Rabs_left; auto.
+      lra.
+  }
+  assert (step2 : forall (r : R), r <> 0 -> (Rabs r / r * (Rabs r / r) = 1)%R).
+  {
+    intros.
+    destruct (Rle_dec 0 r).
+    - rewrite Rabs_right; auto.
+      field.
+      assumption.
+      apply Rle_ge.
+      assumption.
+    - apply Rnot_le_lt in n.
+      rewrite Rabs_left; auto.
+      field.
+      assumption.
+  }
+  rewrite step2; auto; rewrite Rmult_1_l.
+  replace ((Cmod x + fst x) / 2 - (Cmod x - fst x) / 2)%R with (fst x) by lra.
+  rewrite Rmult_comm; rewrite Rmult_assoc.
+  rewrite <- sqrt_mult; auto.
+  replace ((Cmod x - fst x) / 2 * ((Cmod x + fst x) / 2))%R with ((Cmod x * Cmod x - fst x * fst x) / 4)%R by lra.
+  unfold Cmod.
+  unfold pow; repeat rewrite Rmult_1_r.
+  rewrite sqrt_sqrt; auto.
+  replace (fst x * fst x + snd x * snd x - fst x * fst x)%R with (snd x * snd x)%R by lra.
+  replace (snd x * snd x / 4)%R with (snd x / 2 * (snd x / 2))%R by lra.
+  destruct (Rge_dec (snd x) 0).
+  - rewrite Rabs_right; auto.
+    rewrite sqrt_square; auto.
+    unfold Rdiv at 1 3; rewrite Rinv_r.
+    rewrite Rmult_1_l.
+    replace (snd x / 2 + snd x / 2)%R with (snd x)%R by lra.
+    destruct x; simpl; reflexivity.
+    assumption.
+    lra.
+  - apply Rnot_ge_lt in n.
+    rewrite Rabs_left; auto.
+    unfold Rdiv at 1 4.
+    rewrite <- Ropp_mult_distr_l.
+    rewrite Rinv_r; auto.
+    replace (snd x / 2 * (snd x / 2))%R with (- (snd x / 2) * (- (snd x / 2)))%R by lra.
+    rewrite sqrt_square; auto.
+    rewrite <- Ropp_mult_distr_r.
+    rewrite Ropp_mult_distr_l, Ropp_involutive, Rmult_1_l.
+    replace (snd x / 2 + snd x / 2)%R with (snd x)%R by lra.
+    destruct x; simpl; reflexivity.
+    lra.
+Qed.
