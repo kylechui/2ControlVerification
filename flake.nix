b261e657a239dc1466160a583dbe75dcb216ab3d
Kyle Chui <48545987+kylechui@users.noreply.github.com>
2024-04-01 15:30:57 -0700

    ci: Update `coq-quantumlib` to 1.4. (#34)
    * ci: Update `coq-quantumlib` to 1.4.
    
    * refactor: Remove unnecessary `coq-lsp` version.

--- flake.nix
+++ flake.nix
@@ -5,12 +5,8 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system}.extend (final: prev: {
-        coq = prev.coq_8_15;
-        coqPackages = prev.coqPackages_8_15;
-      });
-      coq-quantumlib-version = "v1.1.0";
-      coq-lsp-version = "v8.15";
+      pkgs = nixpkgs.legacyPackages.${system};
+      coq-quantumlib-version = "v1.4.0";
     in let
       coq-quantumlib = pkgs.coqPackages.mkCoqDerivation {
         pname = "quantumlib";
@@ -19,37 +15,10 @@
 
         defaultVersion = coq-quantumlib-version;
         release.${coq-quantumlib-version} = {
-          rev = "71b5b228baa6e2f0c06cc73416ac4a9820f1fd4f";
-          sha256 = "sha256-evIxqyjiOc+HkIl9Ft+7DZHYx74v7UGG5mHg6dlG6oA=";
+          rev = "80018571961e13968bfd80ea9cb061d9469b0817";
+          sha256 = "sha256-mx74GxIwF6mgmrELmNl0l3GiBBf/WrpouHfszylhTYQ=";
         };
         useDune = true;
-      };
-
-      coq-lsp = pkgs.coqPackages.mkCoqDerivation {
-        pname = "lsp";
-        owner = "jishnusen";
-        repo = "coq-lsp";
-        defaultVersion = coq-lsp-version;
-        release.${coq-lsp-version} = {
-          rev = "1c5b5445c4f5b81ce2ab69cb9febb0c2318c9ac6";
-          sha256 = "sha256-7/K1BmN3IuPUPwfoFHO26LOugi3sbhHdwESY0Xotcco=";
-        };
-        installPhase = ''
-          runHook preInstall
-          dune install coq-lsp --prefix=$out
-          wrapProgram $out/bin/coq-lsp --prefix OCAMLPATH : $OCAMLPATH
-          runHook postInstall
-        '';
-        useDune = true;
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        propagatedBuildInputs = [ pkgs.coqPackages.serapi ]
-          ++ (with pkgs.coq.ocamlPackages; [
-            camlp-streams
-            dune-build-info
-            menhir
-            uri
-            yojson
-          ]);
       };
     in {
       devShell.${system} = pkgs.mkShell {
@@ -59,7 +28,7 @@
           pkgs.coq.ocamlPackages.dune_3
           coq-quantumlib
         ] ++ pkgs.lib.optional (builtins.getEnv "CI" != "true")
-          coq-lsp; # Don't build the LSP in GitHub Action
+          pkgs.coqPackages.coq-lsp; # Don't build the LSP in GitHub Action
       };
     };
 }
