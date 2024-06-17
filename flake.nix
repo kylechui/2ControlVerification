aaebf3f08eb99041d709542cf7cba2c9fb9371f9
Kyle Chui <48545987+kylechui@users.noreply.github.com>
2024-06-16 19:12:52 -0700

    feat: Update `coq-lsp`. (#54)

--- flake.nix
+++ flake.nix
@@ -1,13 +1,23 @@
 {
   description = "Local development environment for 2ControlVerification";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+    }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
       coq-quantumlib-version = "v1.4.0";
-    in let
+    in
+    let
       coq-quantumlib = pkgs.coqPackages.mkCoqDerivation {
         pname = "quantumlib";
         owner = "inQWIRE";
@@ -20,15 +30,19 @@
         };
         useDune = true;
       };
-    in {
+    in
+    {
       devShell.${system} = pkgs.mkShell {
-        packages = [
-          pkgs.coq
-          pkgs.coq.ocamlPackages.ocaml
-          pkgs.coq.ocamlPackages.dune_3
-          coq-quantumlib
-        ] ++ pkgs.lib.optional (builtins.getEnv "CI" != "true")
-          pkgs.coqPackages.coq-lsp; # Don't build the LSP in GitHub Action
+        packages =
+          [
+            pkgs.coq
+            pkgs.coq.ocamlPackages.ocaml
+            pkgs.coq.ocamlPackages.dune_3
+            coq-quantumlib
+          ]
+          ++ pkgs.lib.optional (builtins.getEnv "CI" != "true") (
+            pkgs.coqPackages.coq-lsp.override { coq = pkgs.coq_8_18; }
+          ); # Don't build the LSP in GitHub Action
       };
     };
 }
